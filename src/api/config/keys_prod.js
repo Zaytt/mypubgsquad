09aa69672d8feb6a21096e6e651d3171a68273c7
Zaytt <ivanchavez.dev@gmail.com>
2023-02-14 20:19:41 -0600

    updated

--- src/api/config/keys_prod.js
+++ src/api/config/keys_prod.js
@@ -1,17 +1,8 @@
-// module.exports = {
-//   mongoURI: process.env.MONGO_URI,
-//   pubgApiKey: process.env.PUBG_KEY,
-//   secretOrKey: process.env.SECRET_OR_KEY,
-//   facebookClient: process.env.FACEBOOK_CLIENT,
-//   facebookSecret: process.env.FACEBOOK_SECRET,
-//   steamApiKey: process.env.STEAM_API_KEY
-// };
 module.exports = {
-  mongoURI: 'mongodb://pubgleader:zayttleyedsulematz1t0@ds125578.mlab.com:25578/mypubgsquad-dev',
-  pubgApiKey:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwMzg0YWNlMC0yZjJmLTAxMzctNjE0ZC01OWY4MWEyNmE3MzIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTUzMzAwMjY4LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Im15cHViZ3NxdWFkIn0.d_l0xzg8BB6J9uI7TAtzDbaHVqWlQWAa8PX98uUUPxg',
+  mongoURI: process.env.MONGO_URI,
+  pubgApiKey: process.env.PUBG_KEY,
   secretOrKey: process.env.SECRET_OR_KEY,
-  facebookClient: '896602814036511',
-  facebookSecret: 'a67c2b53f06bf4aa776b6eccc2ef07d6',
-  steamApiKey: '1CC72C1C2E54B716CBB9F8497A9410E4'
+  facebookClient: process.env.FACEBOOK_CLIENT,
+  facebookSecret: process.env.FACEBOOK_SECRET,
+  steamApiKey: process.env.STEAM_API_KEY
 };
